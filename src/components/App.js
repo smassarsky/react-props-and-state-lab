3fa4b35fa24ba37daf1ea797ca2e88af95f15131
smassarsky <seth.massarsky@gmail.com>
2021-02-11 16:35:10 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,39 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (e) => {
+    e.persist()
+    this.setState((prevState) => {
+      return {
+        ...prevState,
+        filters: {
+          type: e.target.value
+        }
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    fetch(this.state.filters.type === 'all' ? '/api/pets' : `/api/pets?type=${this.state.filters.type}`)
+    .then(resp => resp.json())
+    .then(json => {
+      this.setState((prevState) => {
+        return {
+          ...prevState,
+          pets: json
+        }
+      })
+    })
+  }
+
+  onAdoptPet = (id) => {
+    this.setState((prevState) => {
+      return {
+        pets: prevState.pets.map(pet => pet.id === id ? {...pet, isAdopted: true}: pet)
+      }
+    })
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +57,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.filters.type === 'all' ? this.state.pets : this.state.pets.filter(pet => pet.type === this.state.filters.type)} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,25 +1,30 @@
 import React from 'react'
 
 class Pet extends React.Component {
+
+  renderButtons = (isAdopted, id) => {
+    return isAdopted ? <button className="ui disabled button">Already adopted</button> : <button onClick={() => this.props.onAdoptPet(id)} className="ui primary button">Adopt pet</button>
+  }
+
   render() {
+    const {gender, name, type, age, weight, id, isAdopted} = this.props.pet
     return (
       <div className="card">
         <div className="content">
           <a className="header">
-            {/*'♀' OR '♂' */}
-            PET NAME
+            {gender === 'male' ? '♂' : '♀'}
+            {name}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{type}</span>
           </div>
           <div className="description">
-            <p>Age: PET AGE</p>
-            <p>Weight: PET WEIGHT</p>
+            <p>Age: {age}</p>
+            <p>Weight: {weight}</p>
           </div>
         </div>
         <div className="extra content">
-          <button className="ui disabled button">Already adopted</button>
-          <button className="ui primary button">Adopt pet</button>
+          {this.renderButtons(isAdopted, id)}
         </div>
       </div>
     )
